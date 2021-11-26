efd0a07429e6a8ddd7ccf06f2c4636aaefb1dd91
liuyuao <2978796630@qq.com>
2021-11-26 15:21:18 +0800

    IDEA提交gitup

---
+++ src/main/java/com/liu/gituptest/GitupTestApplication.java
@@ -0,0 +1,13 @@
+package com.liu.gituptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitupTestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("12");
+    }
+
+}
---
+++ src/test/java/com/liu/gituptest/GitupTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.gituptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitupTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
