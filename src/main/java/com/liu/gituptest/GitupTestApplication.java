0aea5c819fc609233e997c55a8bb2a24fb776669
liuyuao <2978796630@qq.com>
2021-11-26 15:41:37 +0800

    IDEA提交gitup3

--- src/main/java/com/liu/gituptest/GitupTestApplication.java
+++ src/main/java/com/liu/gituptest/GitupTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitupTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("1234");
     }
 
 }
