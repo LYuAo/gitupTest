b360a2e32c9672e9a3134b988966925563894c9f
liuyuao <2978796630@qq.com>
2021-11-29 09:27:24 +0800

    liu提交User类

--- src/main/java/com/liu/gituptest/User.java
+++ src/main/java/com/liu/gituptest/User.java
@@ -3,6 +3,15 @@ package com.liu.gituptest;
 public class User {
     private int id;
     private String name;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public int getId() {
         return id;
